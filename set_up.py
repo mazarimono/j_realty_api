74b4b236b07c33acd0a8d967ec71439114bff857
hideyuki ogawa <mazarimono@gmail.com>
2023-09-08 17:56:42 +0900

    add set_up.py

---
+++ set_up.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prop_api',
+    version=0.1,
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'requests'
+    ]
+)
