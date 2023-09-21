42fbebb59e9f90d98e314b9afbb2bcbc92f4ea9d
hideyuki ogawa <mazarimono@gmail.com>
2023-09-21 15:07:32 +0900

    chg version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='j_realty_api',
-    version=0.0.1,
+    version=0.1,
     packages=find_packages(),
     install_requires=[
         'pandas',
